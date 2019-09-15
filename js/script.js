12fd6d033ceb4f104a1747fc2ea69386cafdc975
NthigaLee <nthigalee@gmail.com>
2019-09-15 15:18:41 +0300

    Real Replica

--- js/script.js
+++ js/script.js
@@ -5,10 +5,18 @@ var player2Acc = [];
 
 
 function player2turn() {
+    $("#oneCss").css("background-color", "#357CBA");
+    $("#twoCss").css("background-color","#BA3535")
     $("#roll-1").attr("disabled", true);
     $("#hold-1").attr("disabled", true);
     $("#roll-2").attr("disabled", false);
     $("#hold-2").attr("disabled", false);
+}
+function player1turn() {
+    $("#roll-2").attr("disabled", true);
+    $("#hold-2").attr("disabled", true);
+    $("#roll-1").attr("disabled", false);
+    $("#hold-1").attr("disabled", false);
 }
 
 
@@ -29,7 +37,9 @@ $(document).ready(function () {
         console.log(player1);
         player1.forEach(function (play1) {
             if (play1 === 1) {
+                
                 totol = 0;
+                player2turn();
                 return (totol);
             }
             else {
@@ -41,26 +51,36 @@ $(document).ready(function () {
         $("#acc1").text(totol);
     });
     $("#hold-1").click(function() {
+        
+        $("#acc1").text(0);
        player1Acc.push(totol);
        console.log(player1Acc);
     var accumu = 0;
        player1Acc.forEach(function(accpl1){
        accumu +=accpl1;
        return (accumu);
-       })
+       });
        
        $(".overallScore1").text(accumu);
+       player2turn();
     });
+
+//player 2 click functions:
+
     $("#roll-2").click(function () {
         var diceNum = _.random(1, 6);
-
-        $("#entries2").text(diceNum);
         total = 0;
+        $("#entries2").text(diceNum);
+        
         player2.push(diceNum);
         console.log(player2);
+        
         player2.forEach(function (play2) {
+            
             if (play2 === 1) {
+                
                 total = 0;
+                player1turn();
                 return (total);
             }
             else {
@@ -72,16 +92,32 @@ $(document).ready(function () {
         $("#acc2").text(total);
     });
     $("#hold-2").click(function() {
+        $("#acc2").text(0);
        player2Acc.push(total);
-       console.log(player2Acc);
+       
     var accumu1 = 0;
-       player2Acc.forEach(function(accpl2){
+       player1Acc.forEach(function(accpl2){
        accumu1 +=accpl2;
        return (accumu1);
-       })
+       });
        
        $(".overallScore2").text(accumu1);
+       player1turn();
     });
+
+    if (accumu >=100 || accumu1 >= 100){
+
+        $(".row").fadeOut(2000);
+        if (accumu >= 100){
+            $(".winner").text("Player 1 Wins!");
+        }
+        else{
+            $(".winner").text("Plaer 2 Wins!!")
+        }
+
+        $(".ending").fadeIn(1000);
+
+    }
 
 
 });
